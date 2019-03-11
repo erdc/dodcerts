55dff5f3abf94fc0585cdb5d940bb98f5cbb659c
Kevin Winters <Kevin.D.Winters@erdc.dren.mil>
2019-03-11 11:43:36 -0500

    dual output matching

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -9,30 +9,25 @@ from unittest import mock
 from dodcerts.dodcerts import __version__
 
 help_msg = [
-    'usage: .* \[-h\] \[-V\]\n',
-    '\n',
-    'dodcerts is a tool that provides the DoD Certificate chain as a PEM bundle.\n',
-    'Returns path to file.\n',
-    '\n',
-    'optional arguments:\n',
-    '  -h, --help     Show this help message and exit.\n',
-    '  -V, --version  Show the dodcerts version number and exit\n',
+    r'usage: .* \[-h\] \[-V\]\n',
+    r'\n',
+    r'dodcerts is a tool that provides the DoD Certificate chain as a PEM bundle.\n',
+    r'Returns path to file.\n',
+    r'\n',
+    r'optional arguments:\n',
+    r'  -h, --help     Show this help message and exit.\n',
+    r'  -V, --version  Show the dodcerts version number and exit\n',
 ]
 
 ver_msg = ['dodcerts ' + __version__]
 
-unrec_msg = [
-    'usage: .* \[-h\] \[-V\]\n',
-    'pytest: error: unrecognized arguments: bad_input\n',
-]
-
-@pytest.mark.parametrize('arg, output', [
-    (['-h'], help_msg),
-    (['--help'], help_msg),
-    (['-V'], ver_msg),
-    (['--version'], ver_msg),
+@pytest.mark.parametrize('arg, output, regex', [
+    (['-h'], help_msg, True),
+    (['--help'], help_msg, True),
+    (['-V'], ver_msg, False),
+    (['--version'], ver_msg, False),
 ])
-def test_parser(arg, output):
+def test_parser(arg, output, regex):
     try:
         from dodcerts.dodcerts.cli import parse_args
     except:
@@ -47,7 +42,11 @@ def test_parser(arg, output):
         res = StringIO(sys.stdout.getvalue())
 
     # compare output
-    for l in output:
-        assert re.match(l, res.readline()) != None
+    if regex:
+        for l in output:
+            assert re.match(l, res.readline()) != None
+    else:
+        for l in output:
+            assert res.readline().find(l) == 0
     # check for end of output
     assert res.readline() == ''
